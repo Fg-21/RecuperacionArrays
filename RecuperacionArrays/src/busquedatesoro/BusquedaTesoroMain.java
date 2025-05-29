46dbb17bd7e767f1c0ca8fb9d93cafe9ecfd6275
Fg-21 <fm.gonzalez@iesnervion.es>
2025-05-29 19:35:21 +0200

    Busqueda DONE

--- RecuperacionArrays/src/busquedatesoro/BusquedaTesoroMain.java
+++ RecuperacionArrays/src/busquedatesoro/BusquedaTesoroMain.java
@@ -2,6 +2,7 @@ package busquedatesoro;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class BusquedaTesoroMain {
 
@@ -10,6 +11,80 @@ public class BusquedaTesoroMain {
 	static int posJ;
 
 	public static void main(String[] args) {
+		// Scanner
+		Scanner rd = new Scanner(System.in);
+
+		// Filas del tablero
+		int filas;
+
+		// Columnas del tablero
+		int col;
+
+		// Numero de obstaculos
+		int obs;
+
+		// Movimiento del jugador
+		String movimiento;
+
+		// Resultado del mov del jugador
+		int mov;
+
+		// Bienvenida al jugador
+		System.out.println("Bienvenido a la busqueda del tesoro");
+
+		do {
+			// Preguntamos el tamaño que quiere el usuario para el tablero
+			System.out.println("Introduce el numero de filas para el tamaño del tablero (No pueden ser menos que 2)");
+			filas = rd.nextInt();
+		} while (filas < 2);
+
+		do {
+			// Preguntamos el tamaño que quiere el usuario para el tablero
+			System.out.println("Introduce el numero de columnas para el tamaño del tablero (No pueden ser menos que 2)");
+			col = rd.nextInt();
+		} while (col < 2);
+
+		// Limpio Buffer
+		rd.nextLine();
+
+		// Calculo los obstaculos en base a las filas y columnas del tablero
+		obs = (int) Math.sqrt(filas * col);
+
+		// Creo el tablero con las filas y columnas pedidas
+		inicializaTablero(filas, col);
+
+		// Genero la posicion del tesoro
+		generaPosicionTesoro();
+
+		// Genera los obstaculos
+		generaObstáculos(obs);
+
+		// Genero la posicion del jugador
+		generaPosicionJugador();
+
+		// Comienza el juego
+		do {
+			// Pinto tablero
+			pintaTablero();
+
+			// Pregunto hacia donde se quiere mover el jugador
+			System.out.println("Donde te quieres mover? (ARRIBA, ABAJO, IZQUIERDA, DERECHA)");
+			movimiento = rd.nextLine();
+			
+			//Intento ejecutar el movimiento
+			mov = mueveJugador(movimiento);
+			
+			//Depende del resultado doy feedback
+			if (mov == -1) {
+				System.out.println("No puedes moverte hacia allí");
+			} else if (mov == -2) {
+				System.out.println("Movimiento no entendido");
+			}
+
+		} while (tablero[posI][posJ] != 'X');
+		
+		//Felicito al jugador
+		System.out.println("Enhorabuena");
 
 	}
 
@@ -61,7 +136,7 @@ public class BusquedaTesoroMain {
 
 			caracter = tablero[posI][posJ];
 
-			if (caracter != ' ' && !obsAround(posI, posJ)) {
+			if (caracter == ' ' && !obsAround(posI, posJ)) {
 				tablero[posI][posJ] = '*';
 				ObsPlaced++;
 			}
@@ -81,15 +156,14 @@ public class BusquedaTesoroMain {
 		int i = (posI - 1) < 0 ? posI + 1 : posI - 1;
 		int j;
 
-		while (i < tablero.length && i <= posI && !obstaculo) {
+		while (i < tablero.length && i <= posI + 1 && !obstaculo) {
 			j = (posJ - 1) < 0 ? posJ + 1 : posJ - 1;
-			while (j < tablero[0].length && j <= posJ && !obstaculo) {
+			while (j < tablero[0].length && j <= posJ +1 && !obstaculo) {
 				if (tablero[i][j] == '*') {
 					obstaculo = true;
 				}
 				j += 2;
 			}
-
 			i += 2;
 		}
 		return obstaculo;
@@ -119,17 +193,14 @@ public class BusquedaTesoroMain {
 	 * posicion del tesoro. Además de pintar al jugador en su sitio correspondiente
 	 */
 	public static void pintaTablero() {
-		//Tabulador inicial
-		System.out.print("\t");
-		
-		//Numeros de indice de columnas
+		// Numeros de indice de columnas
 		for (int j = 0; j < tablero[0].length; j++) {
-			System.out.print(j + "\t");
+			System.out.print("\t" + j);
 		}
-		
+
 		System.out.println();
-		
-		//Tablero con el numero de indice de fila
+
+		// Tablero con el numero de indice de fila
 		for (int i = 0; i < tablero.length; i++) {
 			System.out.print(i + "\t");
 			for (int j = 0; j < tablero[0].length; j++) {
@@ -137,10 +208,46 @@ public class BusquedaTesoroMain {
 					System.out.print("\t");
 				} else if (i == posI && j == posJ) {
 					System.out.print("J\t");
+				} else {
+					System.out.print(tablero[i][j] + "\t");
 				}
 			}
 			System.out.println();
 		}
-		
+
+	}
+
+	public static int mueveJugador(String movimiento) {
+		int res = -1;
+
+		switch (movimiento.toUpperCase()) {
+		case "ARRIBA" -> {
+			if (posI - 1 >= 0 && tablero[posI - 1][posJ] != '*') {
+				posI--;
+				res = 0;
+			}
+		}
+		case "ABAJO" -> {
+			if (posI + 1 < tablero.length && tablero[posI + 1][posJ] != '*') {
+				posI++;
+				res = 0;
+			}
+		}
+		case "DERECHA" -> {
+			if (posJ + 1 < tablero[0].length && tablero[posI][posJ + 1] != '*') {
+				posJ++;
+				res = 0;
+			}
+		}
+		case "IZQUIERDA" -> {
+			if (posJ - 1 >= 0 && tablero[posI][posJ - 1] != '*') {
+				posJ--;
+				res = 0;
+			}
+		}
+		default -> res = -2;
+		}
+
+		return res;
 	}
 }
